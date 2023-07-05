8b1b318459d12fc6e9122810d735698d428b1c3b
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-07-05 23:51:21 +0200

    whatsNew section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f6e25401e354de6c"; }
+/******/ 		__webpack_require__.h = function() { return "f4adbf748690cb7c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
